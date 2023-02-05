7769372280f57568b35fc5b3a83c2d0e3728819d
stormstina <stina.nerin@gmail.com>
2023-02-05 12:37:47 +0100

    Basic setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const addToDoButton = document.getElementById("addToDo");
+
+const clearBtn = document.querySelector("#clearBtn");
+
+const toDoList = document.getElementById("toDoContainer");   
+
+const inputField = document.getElementById("inputField");  
+
+
