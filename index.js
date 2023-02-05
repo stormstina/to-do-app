1dbfe8ca4ce9296a5c96109b31749f2d2e3f4abf
stormstina <stina.nerin@gmail.com>
2023-02-05 13:06:25 +0100

    addTodo-func, EventListener

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ const toDoList = document.getElementById("toDoContainer");
 
 const inputField = document.getElementById("inputField");  
 
+function makeTodo(text) {
 
+    return toDoList.innerHTML +=  `<li>${text}</li>`;
+
+};
+
+addToDoButton.addEventListener("click", () => {
+
+    makeTodo(inputField.value)
+
+})
