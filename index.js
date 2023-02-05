604d6d200c54489f0f0b40e3d3ba95c9032eb14e
stormstina <stina.nerin@gmail.com>
2023-02-05 14:13:54 +0100

    Get current Date

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const toDoList = document.getElementById("toDoContainer");
 
 const inputField = document.getElementById("inputField");  
 
+const dateSpan = document.querySelector("#date");
+
 
 // Om todoArray existerar i local - Hämta datan
 // Om todoArray ej existerar i local - Skapa tom array
@@ -29,6 +31,20 @@ function storeTodo(text) {
     localStorage.setItem("todos", JSON.stringify(todoArray));
 };
 
+function displayDate () {
+    let date = new Date();
+    console.log(date);
+
+    date = date.toString().split(" ");
+    console.log(date);
+
+    dateSpan.innerHTML = date[1] + " " + date[2] + " " + date [3];
+    console.log(dateSpan);
+
+}
+
+window.onload = displayDate();
+
 addToDoButton.addEventListener("click", () => {
 
     makeTodo(inputField.value)
@@ -42,3 +58,4 @@ clearBtn.addEventListener("click", () => {
     toDoList.innerHTML = "";
     localStorage.clear();
 })
+
